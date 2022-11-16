a1f3762076916fceea58474a74620f1eb64be064
Srivatsan <sbalaji2797@gmail.com>
2022-11-16 13:47:56 -0800

    commiting in prod branch

--- Code/python-goof/app/main.py
+++ Code/python-goof/app/main.py
@@ -46,7 +46,7 @@ def _configure_bundles():
             if asset_file.endswith('.js'):
                 js.append(asset_path)
             elif asset_file.endswith('.css'):
-                css.append(asset_path)
+                css.append(asset_path) #adding a css loop
 
     assets = Environment(app)
     if app.debug:
