6b39b67245ae2069ff172da7af402cc587cee5a9
vberegov <79021506+vberegov@users.noreply.github.com>
2024-08-26 20:17:20 -0400

    Update split.sh

--- split.sh
+++ split.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Define the input file
-input_file="files_list"
-
-#Run jq to generate structured informative sbom form original
-mv spdx*.spdx.json sbom.json;
-jq '.sbom.packages[] | {"sbom":{packagename:.name,versionInfo:.versionInfo,licenseDeclared:.licenseDeclared,downloadLocation:.downloadLocation,licenseConcluded:.licenseConcluded,copyrightText:.copyrightText,SPDXID:.SPDXID'}}' sbom.json > sbom_output.json
+mv spdx*.spdx.json sbom.json
 ls -lA
