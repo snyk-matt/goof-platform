7069db8d2529f3caf8c3a00311cfc2886c8350a4
vberegov <79021506+vberegov@users.noreply.github.com>
2024-08-26 20:14:45 -0400

    Update split.sh

--- split.sh
+++ split.sh
@@ -4,6 +4,6 @@
 input_file="files_list"
 
 #Run jq to generate structured informative sbom form original
-mv spdx*.spdx.json sbom.json
+mv spdx*.spdx.json sbom.json;
 jq '.sbom.packages[]? | {"sbom":{packagename:.name,versionInfo:.versionInfo,licenseDeclared:.licenseDeclared,downloadLocation:.downloadLocation,licenseConcluded:.licenseConcluded,copyrightText:.copyrightText,SPDXID:.SPDXID'}}' sbom.json > sbom_output.json
 ls -lA
