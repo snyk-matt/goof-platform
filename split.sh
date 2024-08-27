686d014c62562a45503997b1b9070e5d21a7c8b7
vberegov <79021506+vberegov@users.noreply.github.com>
2024-08-26 20:08:56 -0400

    Update split.sh

--- split.sh
+++ split.sh
@@ -16,9 +16,3 @@ mkdir sbom_chunks
 split -l 11 sbom_output.json sbom_chunks/sbom
 ls sbom_chunks/ > files_list
 echo "split completed"
-
-# Read the file line by line
-#while IFS= read -r line || [ -n "$line" ]; do
-    # Execute the command in each line
-#    curl -X POST 'https://ingress.coralogix.com/webhooks/v1/_hLsZCzkHFHkxB8sIXKbN?cx-api-key=cxtp_O8rQunc7vOLXpjVFaIRP6ifPA83VD1&cx-application-name=github&cx-subsystem-name=sbom' -H 'Content-Type: application/json' -d @"sbom_chunks/""$line" -v
-#done < "$input_file"
