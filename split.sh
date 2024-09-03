b5cc78825830c34af16e9dd6a4389e68c2c509af
vberegov <79021506+vberegov@users.noreply.github.com>
2024-09-03 13:10:54 -0400

    Update split.sh

--- split.sh
+++ split.sh
@@ -6,19 +6,20 @@ mv spdx*.spdx.json sbom_temp.json
 ls -lA
 cat sbom_temp.json | jq -r '.' > sbom.json
 ls -lA
-jq '.packages[]? | {"sbom":{packagename:.name,versionInfo:.versionInfo,licenseDeclared:.licenseDeclared,downloadLocation:.downloadLocation,licenseConcluded:.licenseConcluded,copyrightText:.copyrightText,SPDXID:.SPDXID,copyrightText:.copyrightText}}' sbom.json > sbom_output.json
-ls -lA
-mkdir sbom_chunks
-ls -lA
-echo "Dir done"
-split -l 12 sbom_output.json sbom_chunks/sbom
-echo "split done should see files"
-ls sbom_chunks/ > files_list
-echo "split completed"
-ls -lA 
-cat files_list
+cat sbom.json
+#jq '.packages[]? | {"sbom":{packagename:.name,versionInfo:.versionInfo,licenseDeclared:.licenseDeclared,downloadLocation:.downloadLocation,licenseConcluded:.licenseConcluded,copyrightText:.copyrightText,SPDXID:.SPDXID,copyrightText:.copyrightText}}' sbom.json > sbom_output.json
+#ls -lA
+#mkdir sbom_chunks
+#ls -lA
+#echo "Dir done"
+#split -l 11 sbom_output.json sbom_chunks/sbom
+#echo "split done should see files"
+#ls sbom_chunks/ > files_list
+#echo "split completed"
+#ls -lA 
+#cat files_list
 # Read the file line by line
-while IFS= read -r line || [ -n "$line" ]; do
-    # Execute the command in each line
-    curl -X POST 'https://ingress.coralogix.com/webhooks/v1/_hLsZCzkHFHkxB8sIXKbN?cx-api-key=cxtp_O8rQunc7vOLXpjVFaIRP6ifPA83VD1&cx-application-name=github&cx-subsystem-name=sbom' -H 'Content-Type: application/json' -d @"sbom_chunks/""$line" -v
-done < "$input_file"
+#while IFS= read -r line || [ -n "$line" ]; do
+#    # Execute the command in each line
+#    curl -X POST 'https://ingress.coralogix.com/webhooks/v1/_hLsZCzkHFHkxB8sIXKbN?cx-api-key=cxtp_O8rQunc7vOLXpjVFaIRP6ifPA83VD1&cx-application-name=github&cx-subsystem-name=sbom' -H 'Content-Type: application/json' -d @"sbom_chunks/""$line" -v
+#done < "$input_file"
