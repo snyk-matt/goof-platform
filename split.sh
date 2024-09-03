a6e82d769bdf67a151bd9e56ad1c4e86a2e7ed76
vberegov <79021506+vberegov@users.noreply.github.com>
2024-09-03 12:42:07 -0400

    Update split.sh

--- split.sh
+++ split.sh
@@ -7,6 +7,9 @@ ls -lA
 jq '.packages[]? | {"sbom":{packagename:.name,versionInfo:.versionInfo,licenseDeclared:.licenseDeclared,downloadLocation:.downloadLocation,licenseConcluded:.licenseConcluded,copyrightText:.copyrightText,SPDXID:.SPDXID}}' sbom.json > sbom_output.json
 ls -lA
 mkdir sbom_chunks
+ls -lA
+echo "Dir done"
 split -l 11 sbom_output.json sbom_chunks/sbom
+echo "split done should see files"
 ls sbom_chunks/ > files_list
 echo "split completed"
