d0cfb499f745620466bd5083d7c00ebd354f8e4f
vberegov <79021506+vberegov@users.noreply.github.com>
2024-09-03 12:40:08 -0400

    Update split.sh

--- split.sh
+++ split.sh
@@ -6,8 +6,6 @@ cat sbom_temp.json | jq -r '.' > sbom.json
 ls -lA
 jq '.packages[]? | {"sbom":{packagename:.name,versionInfo:.versionInfo,licenseDeclared:.licenseDeclared,downloadLocation:.downloadLocation,licenseConcluded:.licenseConcluded,copyrightText:.copyrightText,SPDXID:.SPDXID}}' sbom.json > sbom_output.json
 ls -lA
-cat -n 20 sbom_output.json
-ls -lA
 mkdir sbom_chunks
 split -l 11 sbom_output.json sbom_chunks/sbom
 ls sbom_chunks/ > files_list
