8a3847ef475a4a8fb93352336ea34fe76c8b0e5d
vberegov <79021506+vberegov@users.noreply.github.com>
2024-09-03 12:29:04 -0400

    Update split.sh

--- split.sh
+++ split.sh
@@ -3,6 +3,8 @@
 mv spdx*.spdx.json sbom_temp.json
 ls -lA
 cat sbom_temp.json | jq -r '.' > sbom.json
+ls -lA
 jq 'try .sbom.packages[]? | {"sbom":{packagename:.name}}' sbom.json > sbom_output.json
 ls -lA
 cat -n 20 sbom_output.json
+ls -lA
